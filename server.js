e1dee80bfa27adf4f8184153a5acd208d9fe4f91
rikian <84202969+rikian@users.noreply.github.com>
2022-06-01 22:20:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const readline = require("readline");
 const mysql2 = require("mysql2");
-require("dotenv").config();
 
 const pool = mysql2.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: process.env.USER_SQL || "your user sql server",
-  password: process.env.SQL_KEY || "your password sql server",
-  // database: "db_belajar",
+  host: "127.0.0.1", //localhost
+  port: 3306, //default port
+  user: "your user sql server",
+  password: "your password sql server",
+  // database: "db_belajar", //uncomment this after create db_belajar
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -20,7 +19,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const command = function () {
+function command() {
   rl.question("input exit for exit\n\nyour sql query : ", async (name) => {
     if (name === "exit") {
       console.log("have nice day");
@@ -41,6 +40,6 @@ const command = function () {
     console.table(await sql());
     command();
   });
-};
+}
 
-command();
+module.exports = { command };
